2f6d2af619bc38cdbcb7feece012ac7ff87c15b8
Luis Alejandro Rodríguez Cárcamo <l.rcarcamo@alumnos.upm.es>
2024-03-19 23:28:14 +0100

    make the application executable issue #X1

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,12 @@
+package hello;
+
+import org.sprintframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	
+	public static void main([] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
